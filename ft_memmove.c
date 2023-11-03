478afd43109d14478b37acf3027167418783bd9c
Jaakko Junttila <jajuntti@student.hive.fi>
2023-11-03 15:22:13 +0200

    Fixed memmove problem was size_t index underflow

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,14 +1,14 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jajuntti <jajuntti@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: jajuntti <jajuntti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 10:08:14 by jajuntti          #+#    #+#             */
-/*   Updated: 2023/10/26 16:04:55 by jajuntti         ###   ########.fr       */
+/*   Updated: 2023/11/03 15:17:20 by jajuntti         ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #include "libft.h"
 
@@ -16,18 +16,18 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	unsigned char	*to;
 	unsigned char	*from;
-	size_t			i;
+	long			i;
 
 	to = (unsigned char *)dst;
 	from = (unsigned char *)src;
 	if (src == dst)
 		return (dst);
-	if (src + len - 1 >= dst && src + len - 1 <= dst + len - 1)
+	if (dst > src && dst < src + len)
 	{
 		i = len - 1;
 		while (i >= 0)
 		{
-			to[i] = from[i];
+			*(to + i) = *(from + i);
 			i--;
 		}
 	}
