5e0ac607f7f3b7f389bb33e419b781d0a10244ac
Jaakko Junttila <jajuntti@student.hive.fi>
2023-11-04 17:52:56 +0200

    Made strjoin use calloc to ensure functionality

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: jajuntti <jajuntti@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 12:29:43 by jajuntti          #+#    #+#             */
-/*   Updated: 2023/10/31 10:19:19 by jajuntti         ###   ########.fr       */
+/*   Updated: 2023/11/04 17:46:15 by jajuntti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	len;
 
 	len = ft_strlen(s1) + ft_strlen(s2);
-	str = malloc(ft_strlen(s1) + len + 1);
+	str = ft_calloc(len + 1, sizeof(char));
 	if (str == NULL)
 		return (NULL);
 	ft_strlcat(str, s1, len + 1);
