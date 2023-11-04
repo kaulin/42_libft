e00b439a503d7016b398a11f7baff799c1e02f48
Jaakko Junttila <jajuntti@student.hive.fi>
2023-11-04 15:52:22 +0200

    Fixed wrong argument type in ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,22 +6,28 @@
 /*   By: jajuntti <jajuntti@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 11:39:01 by jajuntti          #+#    #+#             */
-/*   Updated: 2023/11/04 11:55:17 by jajuntti         ###   ########.fr       */
+/*   Updated: 2023/11/04 15:44:24 by jajuntti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strncmp(char *s1, char *s2, int n)
+#include "libft.h"
+
+int	ft_strncmp(char *s1, char *s2, size_t n)
 {
 	int	i;
+	unsigned char *str1;
+	unsigned char *str2;
 
 	i = 0;
-	while (s1[i] && s2[i] && i < n)
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	while (str1[i] && str2[i] && i < n)
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+		if (str1[i] != str2[i])
+			return (str1[i] - str2[i]);
 		i++;
 	}
 	if (i < n)
-		return (s1[i] - s2[i]);
+		return (str1[i] - str2[i]);
 	return (0);
 }
