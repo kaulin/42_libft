bb143fb18997d14cf9a59a6142b0859ccac7a9b5
Jaakko Junttila <jajuntti@student.hive.fi>
2023-11-04 15:53:19 +0200

    Fixed return value and added checker for 0 dest.

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: jajuntti <jajuntti@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 16:47:38 by jajuntti          #+#    #+#             */
-/*   Updated: 2023/11/04 11:55:31 by jajuntti         ###   ########.fr       */
+/*   Updated: 2023/11/04 13:16:10 by jajuntti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,20 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 
 	dst_i = 0;
 	src_i = 0;
-	len = ft_strlen(dst) + ft_strlen(src);
+	len = ft_strlen(src);
 	while (dst[dst_i])
 		dst_i++;
-	while (dst_i < (dstsize - 1) && src[src_i])
-	{
-		dst[dst_i] = src[src_i];
-		dst_i++;
-		src_i++;
-	}
-	dst[dst_i] = 0;
+	if (dst_i < dstsize)
+		len += dst_i;
+	else
+		len += dstsize;
+	if (dstsize > 0)
+		while (dst_i < (dstsize - 1) && src[src_i])
+		{
+			dst[dst_i] = src[src_i];
+			dst_i++;
+			src_i++;
+		}
+		dst[dst_i] = 0;
 	return (len);
 }
