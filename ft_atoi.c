862b6f2e241227ceebb73848ba2c6a1c8389e954
Jaakko Junttila <jajuntti@student.hive.fi>
2023-11-07 16:04:18 +0200

    Fixed atoi overflow!

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,22 +6,22 @@
 /*   By: jajuntti <jajuntti@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 11:39:44 by jajuntti          #+#    #+#             */
-/*   Updated: 2023/11/04 11:54:56 by jajuntti         ###   ########.fr       */
+/*   Updated: 2023/11/07 16:03:50 by jajuntti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// Integer overflow!!!!
 int	ft_atoi(const char *str)
 {
-	long	i;
 	long	num;
+	long	prev;
+	int		i;
 	int		sign;
 
 	i = 0;
 	num = 0;
-	sign = 0;
+	sign = 1;
 	while (str[i] && ft_strchr(" \t\v\n\r\f", str[i]) != NULL)
 		i++;
 	if (str[i] == '-')
@@ -30,11 +30,13 @@ int	ft_atoi(const char *str)
 		i++;
 	while (str[i] && ft_isdigit(str[i]))
 	{
-		if (sign < 0)
-			num = num * 10 - (str[i] - '0');
-		else
-			num = num * 10 + (str[i] - '0');
+		prev = i;
+		num = num * 10 + (long)str[i] - '0';
+		if (num < prev && sign == 1)
+			return (-1);
+		if (num > prev && sign == -1)
+			return (0);
 		i++;
 	}
-	return ((int)num);
+	return (sign * (int)num);
 }
