3e1c6462dee68d9ac9dd7434220526fc1c6c7860
Jaakko Junttila <jajuntti@student.hive.fi>
2023-11-08 11:49:57 +0200

    Synced ft_split and ft_substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: jajuntti <jajuntti@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 11:57:46 by jajuntti          #+#    #+#             */
-/*   Updated: 2023/11/08 10:34:09 by jajuntti         ###   ########.fr       */
+/*   Updated: 2023/11/08 11:48:40 by jajuntti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*substr;
 
-	if (s == NULL)
-		return (NULL);
 	if (len == 0 || start > ft_strlen(s))
 	{
 		substr = malloc(1);
