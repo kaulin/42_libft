84b5b6d20db23d6701d689f99d21912f1acdd66c
Jaakko Junttila <jajuntti@student.hive.fi>
2023-11-13 14:04:28 +0200

    Fixes strlcat, now segfaults as intended <3

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: jajuntti <jajuntti@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 16:47:38 by jajuntti          #+#    #+#             */
-/*   Updated: 2023/11/04 17:18:04 by jajuntti         ###   ########.fr       */
+/*   Updated: 2023/11/13 13:56:53 by jajuntti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,26 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	size_t	dst_i;
-	size_t	src_i;
+	size_t	i;
 	size_t	len;
 
-	dst_i = 0;
-	src_i = 0;
-	len = ft_strlen(src);
-	while (dst[dst_i])
-		dst_i++;
-	if (dst_i < dstsize)
-		len += dst_i;
+	i = 0;
+	if (dst == NULL && dstsize == 0)
+		return (ft_strlen(src));
 	else
-		len += dstsize;
-	if (dstsize > 0)
+		len = ft_strlen(dst);
+	if (len >= dstsize)
+		return (dstsize + ft_strlen(src));
+	if (dstsize > 1)
 	{
-		while (dst_i < (dstsize - 1) && src[src_i])
+		while (src[i] && len + i < dstsize - 1)
 		{
-			dst[dst_i] = src[src_i];
-			dst_i++;
-			src_i++;
+			dst[len + i] = src[i];
+			i++;
 		}
-		dst[dst_i] = 0;
+		dst[len + i] = 0;
 	}
-	return (len);
+	else if (len == 1)
+		dst[0] = 0;
+	return (len + ft_strlen(src));
 }
