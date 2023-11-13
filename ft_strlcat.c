3dc77ae8aeda77f32cf31d5f177bee3d6811f4ee
Jaakko Junttila <jajuntti@student.hive.fi>
2023-11-13 16:14:15 +0200

    Removes redundant conditions

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: jajuntti <jajuntti@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 16:47:38 by jajuntti          #+#    #+#             */
-/*   Updated: 2023/11/13 13:56:53 by jajuntti         ###   ########.fr       */
+/*   Updated: 2023/11/13 16:13:10 by jajuntti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	size_t	len;
 
 	i = 0;
-	if (dst == NULL && dstsize == 0)
+	if (dstsize == 0)
 		return (ft_strlen(src));
 	else
 		len = ft_strlen(dst);
@@ -33,7 +33,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 		}
 		dst[len + i] = 0;
 	}
-	else if (len == 1)
+	else
 		dst[0] = 0;
 	return (len + ft_strlen(src));
 }
