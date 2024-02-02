feb5a413569c2558e73c25ddf34f47d5de697c06
Jaakko Junttila <jajuntti@student.hive.fi>
2024-02-02 08:20:51 +0200

    Fixes one include

--- ft_get_next_line.c
+++ ft_get_next_line.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "ft_get_next_line.h"
 
 static int	check_params(int fd, char *buffer)
 {
